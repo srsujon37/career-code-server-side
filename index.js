e1a447ec291ec599c8354b62b188a943788868b2
srsujon37 <srsujon97@gmail.com>
2025-05-30 16:00:12 +0600

    deploy

--- index.js
+++ index.js
@@ -48,6 +48,16 @@ async function run() {
         applicant: email
       }
       const result = await applicationCollection.find(query).toArray()
+
+      // bad way to aggregate data
+      for(const application of result){
+        const jobId = application.jobId;
+        const jobQuery = { _id: new ObjectId(jobId)}
+        const job = await jobsCollection.findOne(jobQuery);
+        application.company = job.company
+        application.title = job.title
+        application.company_logo = job.company_logo
+      }
       res.send(result)
     })
 
