8cc630f0e8a9daa959f09d6f080b4fec22bdcac2
srsujon37 <srsujon97@gmail.com>
2025-05-31 23:02:12 +0600

    deploy

--- index.js
+++ index.js
@@ -32,12 +32,39 @@ async function run() {
     const jobsCollection = client.db('careerCode').collection('jobs');
     const applicationCollection = client.db('careerCode').collection('applications');
 
+    // view applications
+    app.get('/applications/job/:job_id', async(req, res) =>{
+      const job_id = req.params.job_id;
+      const query = { jobId: job_id}
+      const result = await applicationCollection.find(query).toArray();
+      res.send(result);
+    })
+    // ViewApplications data
+    app.patch('/applications/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          status: req.body.status
+        }
+      }
+      const result = await applicationCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
     // job applications related aPIs
     app.post('/applications', async(req, res) => {
       const application = req.body;
       console.log(application);
       const result = await applicationCollection.insertOne(application);
       res.send(result)
+    })
+// 
+    app.post('/jobs', async(req, res) =>{
+      const newJob = req.body;
+      console.log(newJob);
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
     })
 
     // job application list data collect
@@ -61,12 +88,42 @@ async function run() {
       res.send(result)
     })
 
-    // job api 
+    // jobs api 
     app.get('/jobs', async(req, res) =>{
+
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
         const cursor = jobsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+
+    app.get('/jobs/applications', async(req, res) =>{
+      const email = req.query.email;
+      const query = { hr_email: email}
+      const jobs = await jobsCollection.find(query).toArray();
+
+      // should use aggregate to have optimum data fetching
+      for(const job of jobs) {
+        const applicationQuery = { jobId: job._id.toString()}
+        const application_count = await applicationCollection.countDocuments(applicationQuery)
+        job.application_count = application_count;
+        res.send(jobs);
+      }
+
+    })
+
+    // ? could be done but should not be done
+    // app.get('/jobsBYEmailAddress', async(req, res) =>{
+    //   const email = req.query.email;
+    //   const query = { hr_email: email }
+    //   const result = await jobsCollection.find(query).toArray()
+    //   res.send(result);
+    // })
+
 
     // single job get for apply btn
     app.get('/jobs/:id', async(req, res) =>{
@@ -75,6 +132,7 @@ async function run() {
         const result = await jobsCollection.findOne(query);
         res.send(result)
     })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
